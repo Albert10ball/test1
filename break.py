d7ab97cbbb41f7afdda4e002e8a4438918a81465
Albert10ball <61967444+Albert10ball@users.noreply.github.com>
2020-03-09 17:58:19 +0800

    Create break.py

---
+++ break.py
@@ -0,0 +1,12 @@
+# encoding=utf-8
+
+lin = "8"
+ling = input("请输入一个我心中想的数字：")
+
+while True:
+    if ling == lin:
+        break
+    ling = input("不好意思，没有猜对我想的哦，请重来：")
+print("真棒，竟然被你猜到了！")
+
+print(222)
