e36d1ef116e18370f499d3f4802186bbe5d3a212
Albert <44149471+yeealbert@users.noreply.github.com>
2020-06-24 10:01:34 +0800

    create new py file

--- requests_test.py
+++ requests_test.py
@@ -2,6 +2,7 @@
 
 import requests
 import json
+import time
 
 
 def seach(kw):
@@ -32,15 +33,37 @@ def seach(kw):
 # print(seach("sda"))
 
 
-url = "http://192.168.1.143:17004/jsonrpc"
+url = "http://beta.api.nerve.network/jsonrpc"
 headers = {"Content-Type": "application/json"}
 
-sender = "TNVTdTSPVcqUCdfVYWwrbuRtZ1oM6GpSgsgF5"
+sender = "TNVTdTSPNEpLq2wnbsBcD8UDTVMsArtkfxWgz"
 
 passwd = "nuls123456"
-amount = 100000000*300000
+amount = 210000\
+         * 100000000
 remark = "test"
-receiverR = ["TNVTdTSPQvEngihwxqwCNPq3keQL1PwrcLbtj"]
+# receiverR = ["TNVTdTSPTXQudD2FBSefpQRkXTyhhtSjyEVAF"]
+
+receiverR = ["TNVTdTSPEgem44Rihm3Dfcjt2JsQj8Gv7Ti5d", "TNVTdTSPW62pTbfi2WLhj96eJKuzuufN2ZweH",
+             "TNVTdTSPPLNvD98UZpubMZcn79N9jHp9zugy2", "TNVTdTSPQ4Cqeih2a2rjCAZUGjrg9UPuNjVbq",
+             "TNVTdTSPFeCiZFse5JbBPB6Kh6vXKNywSMZra", "TNVTdTSPViRnvQwwACbFt3jGXrRHbnzArT9rU",
+             "TNVTdTSPMF5zy7FnKWY5mtryfCGP9CyjXTZ9i", "TNVTdTSPT9cL5AXEqVnW2WeaGY5kjzEeTkvdC",
+             "TNVTdTSPPwSttPL8jiprbtxSrw4fDJk6zfAvn", "TNVTdTSPPnSqgzo6zmZvzLiJn9poRKp7mrV4M",
+             "TNVTdTSPUTmdXoaZ2gMCsttHGDKeLSstPfi8B", "TNVTdTSPJqqUawEp8h4zScWNjQN6Jkhrh2oSN",
+             "TNVTdTSPNRVTRuhNDhQfE5WBRHmB31KwQ2h99", "TNVTdTSPQJDiXSfREgaDCxhDHg5RWuv1MJpaG",
+             "TNVTdTSPP5AV2zE1eFvycGZjWmZ9EbjwAEUys", "TNVTdTSPHVuXErqCPjuak7xD3dJVcXoLWHTQV",
+             "TNVTdTSPTWGuRcYw2MxPnSR5aFD7ZWqTgeaTx", "TNVTdTSPGkKuAEbScbbzaDdWdjJ6dK5hT6ysV",
+             "TNVTdTSPVt8ni6vBhTc4hy6MaeC4G92ncxGDg", "TNVTdTSPTiRkzWj1FvPWJUqp6HPCiMM5aWd8Y",
+             "TNVTdTSPGWFtSVGE8GBFExXiRVPCbkSTwmqf5", "TNVTdTSPP2fQMhQtAXXhoTig6Stcm56xhY1HC",
+             "TNVTdTSPStMhYR8mpbCQJjad3Xd7EMSkG4Z7Z", "TNVTdTSPJKE5RqxqpxBnXtcvWEd9iMZieCz2w",
+             "TNVTdTSPM2EpH7DrKttaqNk3nSQY7mQ7oLy3T", "TNVTdTSPNGzp1M5vfs35K97YEnmhvoJnvx7CV",
+             "TNVTdTSPS5kvh5bVf7amiiyFPP9pPPnbxTyP5", "TNVTdTSPKsMZoHQi7v8YrznmZRc9pg91krNkj",
+             "TNVTdTSPFH6U488iVPd3BnF2QXDWDEHP1M4Qt", "TNVTdTSPRtbNCS1uuhRGNWjqdsiCKgfmf6EjT",
+             "TNVTdTSPJJEoFM6Wy2pkAzeJomUBXu4qPbQzp", "TNVTdTSPJPE4qmyAsAvDu52JVPs685tTGR9dE",
+             "TNVTdTSPEqnXW9Rhy3uAR3NGLHcFwZzjxABJj", "TNVTdTSPQXztfyaJU9WyduYsUDkBUoRWbSaYo",
+             "TNVTdTSPHHrTDj7ABetDKCqdmR8rGmxSjDeq5", "TNVTdTSPJZFP9joszvdCWXqm2MDqYmqUfxEA9",
+             "TNVTdTSPVej3zFKYpRHRhZAR5TQ8a53nQX2sW", "TNVTdTSPLf9qWCZBtcY24FufxQBqD7HVEMs4v",
+             "TNVTdTSPQ38RGd55Pr3ZS1sS9XUysUzixg8ps", "TNVTdTSPPXXC7jxHCBgkBHkFpe4r16ynUjKRc"]
 
 
 def post(url, headers, receiver):
@@ -70,11 +93,13 @@ def post(url, headers, receiver):
 lenth = len(receiverR)
 print(lenth)
 for i in range(0, lenth):
-    print(receiverR[i])
+    print(receiverR[i], i)
     print(post(url, headers, receiverR[i]))
+    time.sleep(1)
 
 # for i in range(0, 9000000):
-#    print("第%d次转账:" % i)
-#   post(url, headers)
+#     print("第%d次转账:" % i)
+#     print(post(url, headers, receiverR[0]))
+#     time.sleep(1)
 
 
--- test.py
+++ test.py
@@ -2,17 +2,59 @@
 
 
 import unittest
+import time
+import datetime
+import requests
+from time import strftime
+
+# class test_1(unittest.TestCase):
+#
+#     def test_test1(self):
+#         self.assertEqual(abs(-20), 20)
+#         # self.assertEqual(abs(-2), 3)
+#
+#
+# if __name__ == '__main__':
+#     unittest.main()
+
+print(time.ctime())
+print(int(time.time()))
+
+url = "https://api.nuls.io/jsonrpc"
+header = {"Content-Type": "application/json"}
 
 
-class test1(unittest.TestCase):
+def get_lastblock(url, header):
 
-    def test_test1(self):
-        self.assertEqual(abs(-20), 20)
-        # self.assertEqual(abs(-2), 3)
+    form_data ={
+        "jsonrpc": "2.0",
+        "method": "getBestBlockHeader",
+        "params": [1],
+        "id": 1234
+    }
+    response = requests.post(url=url, json=form_data, headers=header)
+    # print(response.text)
+    return response.json()
 
 
-if __name__ == '__main__':
-    unittest.main()
+response = get_lastblock(url, header)
+height1 = response["result"]["height"]
+block = 2620000 - height1
+print(block)
+for i in range(0, block):
 
-
-
+    res = get_lastblock(url, header)
+    height = res["result"]["height"]
+    ctime = res["result"]["time"]
+    cha = 2620000 - height
+    time1 = (cha * 10)
+    time2 = int(time.time())
+    now = datetime.datetime.now()
+    t = time1 + time2
+    timeArray = time.localtime(t)
+    print("当前区块时间为：", ctime)
+    print("当前NULS主网高度为：%d" % height)
+    print("预计快照主网高度为：2620000")
+    print("预计快照时间为：", time.strftime("%Y-%m-%d %H:%M:%S", timeArray))
+    print("*****************************************")
+    time.sleep(10)
